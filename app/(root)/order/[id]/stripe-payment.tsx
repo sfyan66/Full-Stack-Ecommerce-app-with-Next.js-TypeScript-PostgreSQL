1237c6f1c6bc965f137e9b4c01c49da43f72f901
sfyan66 <sfyan77510@gmail.com>
2026-01-15 18:06:51 +0300

    add stripe payment

--- app/(root)/order/[id]/stripe-payment.tsx
+++ app/(root)/order/[id]/stripe-payment.tsx
@@ -9,9 +9,10 @@ import {
 } from "@stripe/react-stripe-js";
 import { useTheme } from "next-themes";
 import { loadStripe } from "@stripe/stripe-js";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { formatCurrency } from "@/lib/utils";
+import { SERVER_URL } from "@/lib/constants";
 
 const StripePayment = ({
   priceInCents,
@@ -28,15 +29,41 @@ const StripePayment = ({
     const stripe = useStripe();
     const elements = useElements();
 
-    const [email, setEmail] = useState();
-    const [isLoading, setIsLoading] = useState(false);
-    const [errorMessage, setErrorMessage] = useState();
+    const [email, setEmail] = useState<string>();
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [errorMessage, setErrorMessage] = useState<string>();
+
+    const handleSubmit = async (e: FormEvent) => {
+      e.preventDefault();
+
+      if (stripe == null || elements == null || email == null) return;
+
+      setIsLoading(true);
+
+      stripe
+        .confirmPayment({
+          elements,
+          confirmParams: {
+            return_url: `${SERVER_URL}/order/${orderId}/stripe-payment-success`,
+          },
+        })
+        .then(({ error }) => {
+          if (
+            error?.type == "card_error" ||
+            error?.type == "validation_error"
+          ) {
+            setErrorMessage(error?.message ?? "Unknown error occured");
+          }
+        })
+        .finally(() => setIsLoading(false));
+    };
 
     return (
-      <form className="space-y-4">
+      <form className="space-y-4" onSubmit={handleSubmit}>
         <div className="text-xl">Stripe Checkout</div>
         {errorMessage && <div className="text-destructive">{errorMessage}</div>}
         <PaymentElement />
+        <LinkAuthenticationElement onChange={(e) => setEmail(e.value.email)} />
         <Button
           className="w-full"
           size="lg"
---
+++ app/api/webhooks/stripe/route.ts
@@ -0,0 +1,33 @@
+import { updateOrderToPaid } from "@/lib/actions/order.actions";
+import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
+
+export async function POST(req: NextRequest) {
+  const event = await Stripe.webhooks.constructEvent(
+    await req.text(),
+    req.headers.get("stripe-signature") as string,
+    process.env.STRIPE_WEBHOOK_SECRET as string
+  );
+
+  if (event.type === "charge.succeeded") {
+    const { object } = event.data;
+
+    await updateOrderToPaid({
+      orderId: object.metadata.orderId,
+      paymentResult: {
+        id: object.id,
+        status: "COMPLETED",
+        email_address: object.billing_details.email!,
+        pricePaid: (object.amount / 100).toFixed(),
+      },
+    });
+
+    return NextResponse.json({
+      message: "updateOrderToPaid was successfully",
+    });
+  }
+
+  return NextResponse.json({
+    message: "event is not charge.succeeded",
+  });
+}
